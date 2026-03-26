7e6563036d0d749e523b7fa9b873e18405bfa8ee
nirnay-dev <work.nirnayraval@gmail.com>
2026-03-26 07:26:10 +0530

    update http server dependency to version 0.208.0

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/std@0.187.0/http/server.ts";
+import { serve } from "https://deno.land/std@0.208.0/http/server.ts";
 
 // Helper function to list all .html files in the current directory
 async function listHtmlFiles(): Promise<string[]> {
